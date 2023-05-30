bf77246a59c5db12ee13bdfb1de013023a48d7f3
Alejio20 <humbleotamiri@gmail.com>
2023-05-30 07:49:24 -0700

    Added binary_tree

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,46 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height_b - Measures height of a binary tree for a bal tree
+ * @tree: tree to go through
+ * Return: the height
+ */
+
+size_t binary_tree_height_b(const binary_tree_t *tree)
+{
+	size_t l = 0;
+	size_t r = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		if (tree)
+		{
+			l = tree->left ? 1 + binary_tree_height_b(tree->left) : 1;
+			r = tree->right ? 1 + binary_tree_height_b(tree->right) : 1;
+		}
+		return ((l > r) ? l : r);
+	}
+}
+
+/**
+ * binary_tree_balance - Measures balance factor of a binary tree
+ * @tree: tree to go through
+ * Return: balanced factor
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int right_bal = 0, left_bal = 0, total_bal = 0;
+
+	if (tree)
+	{
+		left_bal = ((int)binary_tree_height_b(tree->left));
+		right_bal = ((int)binary_tree_height_b(tree->right));
+		total_bal = left_bal - right_bal;
+	}
+	return (total_bal);
+}
